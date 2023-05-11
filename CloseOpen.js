21cda77472c7f708c8dcb50b898a9ce755730b73
dwil098 <123797422+dwil098@users.noreply.github.com>
2023-05-11 16:01:20 +0800

    Create CloseOpen.js

---
+++ CloseOpen.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Toggle Button</title>
+</head>
+<body>
+  <button id="toggle-button" onclick="myFunction()">Open</button>
+  <script>
+function myFunction() {
+  document.getElementById("toggle-button").innerHTML = "Close";
+}
+</script>
+
+</body>
+</html>
